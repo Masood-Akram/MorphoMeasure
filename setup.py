98e21b165dc1fd47271db4205248048617c7c807
Masood-Akram <masoodakram@mednet.ucla.edu>
2025-07-21 09:54:18 -0700

    updated files

--- setup.py
+++ setup.py
@@ -3,17 +3,29 @@ from setuptools import setup, find_packages
 setup(
     name="morphomeasure",
     version="0.1.0",
-    description="Python wrapper and CLI for L-Measure morphometric feature extraction",
+    description="Python package and CLI for L-Measure-based morphometric extraction",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Masood Akram",
+    author_email="masood.ahmed.akram@gmail.com",
+    url="https://github.com/Masood-Akram/MorphoMeasure",
     packages=find_packages(),
     install_requires=[
-        "pandas"
+        "pandas",
     ],
-    python_requires=">=3.7",
     include_package_data=True,
+    package_data={
+        # No need for this if using MANIFEST.in, but doesn't hurt
+    },
     entry_points={
         'console_scripts': [
-            'morphomeasure=morphomeasure.cli:main'
-        ]
+            'morphomeasure=morphomeasure.cli:main',
+        ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"#,
+        # "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
 )
