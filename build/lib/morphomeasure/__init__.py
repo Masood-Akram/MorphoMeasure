ded3a89e6f8773da435527147c68ee9af1e49215
Masood-Akram <masoodakram@mednet.ucla.edu>
2025-07-30 12:08:06 -0700

    updated v0.1.1

--- build/lib/morphomeasure/__init__.py
+++
@@ -1,2 +0,0 @@
-from .lmwrapper import LMeasureWrapper
-
--- morphomeasure/__init__.py
+++ morphomeasure/__init__.py
@@ -1,7 +1,10 @@
+__version__ = "0.1.1"
+
 from .lmwrapper import LMeasureWrapper
 """
 Initializes the MorphoMeasure package by importing the LMeasureWrapper class from the lmwrapper module.
 Imports:
     LMeasureWrapper (from .lmwrapper): A wrapper class for LMeasure functionality.
 """
+
 
