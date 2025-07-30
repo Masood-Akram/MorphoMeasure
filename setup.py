30f2a9f79cb0e65765ec77f48b4fa2e49cf889f7
Masood-Akram <masoodakram@mednet.ucla.edu>
2025-07-30 11:40:15 -0700

    morphomeasure v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="morphomeasure",
-    version="0.1.0",
+    version="0.1.1",
     description="Python package and CLI for L-Measure-based morphometric extraction",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
